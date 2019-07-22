a0da273005088d6ea68f0b27cfd7a820f9165215
Nagkumar Arkalgud <nagkumar@Nagkumars-iMac.local>
2019-07-22 16:27:54 -0700

    Trying to figure out multiple jobs

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-virtualenv .env -p python3
-source ./.env/bin/activate
+. venv/bin/activate
 cd lmk
 python manage.py collectstatic --no-input
 gsutil rsync -r static/ gs://lmk-static/static
-pip install -r requirements.txt
 docker build -t gcr.io/letmeknow-243504/lmk .
 docker push gcr.io/letmeknow-243504/lmk
 kubectl apply -f lmk.yaml
