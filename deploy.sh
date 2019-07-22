3827d8bb683bf97c6fad0147868f4e402834aca1
Nagkumar Arkalgud <nagkumar@Nagkumars-iMac.local>
2019-07-22 16:43:27 -0700

    Trying to figure out deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 . venv/bin/activate
 cd lmk
 python manage.py collectstatic --no-input
-gsutil rsync -r static/ gs://lmk-static/static
+gsutil rsync -rm static/ gs://lmk-static/static
 docker build -t gcr.io/letmeknow-243504/lmk .
 docker push gcr.io/letmeknow-243504/lmk
 kubectl apply -f lmk.yaml
